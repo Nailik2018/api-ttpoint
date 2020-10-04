8338fc0778e8c5a7db6f9299f1748130aa190d96
SURFACE-BOOK-KI\kilua <kilualthaus@hotmail.com>
2020-10-04 18:27:04 +0200

    Aufbau API index.html, Auflistung aller verbände als JSON per URL aufrufbar

---
+++ api/get-all-associations.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once "../collections/DataBase.php";
+
+header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *");
+
+$db = new DataBase();
+$db->connection();
+
+print_r($db->sqlSelectStatement("SELECT * FROM association"))
+
+
+?>
---
+++ collections/DataBase.php
@@ -0,0 +1,85 @@
+<?php
+
+class DataBase{
+
+    public $dbname;
+    public $username;
+    public $password;
+    public $pdo;
+
+    public function connection(){
+
+        $databaseIni = parse_ini_file("../configurations/database.ini");
+
+        $dbuser = $databaseIni['dbuser'];
+        $db = $databaseIni['db'];
+        $password = $databaseIni['password'];
+
+        $this->dbname = $db;
+        $this->username = $dbuser;
+        $this->password = $password;
+        $this->pdo = new PDO($this->dbname, $this->username, $this->password);
+    }
+
+    public function sqlSelectStatement($sqlStatement){
+
+        $pdo = $this->pdo;
+
+        $statement = $pdo->prepare($sqlStatement);
+        $allPlayers = '';
+
+        if($statement->execute()){
+            $allPlayers = $statement->fetchAll(PDO::FETCH_ASSOC);
+        }else{
+            error_log("Die Funktion sqlSelectStatement()konnte nicht ausgeführt werden!");
+        }
+
+        $allPlayers = json_encode($allPlayers);
+
+        return $allPlayers;
+    }
+
+    public function sqlPreparedStatement($sqlStatement, $bindParams){
+
+        $pdo = $this->pdo;
+
+        $statement = $pdo->prepare($sqlStatement);
+
+        foreach ($bindParams as $key => &$value){
+            $statement->bindParam(":$key", $value);
+        }
+
+        if($statement->execute()){
+            $informations = $statement->fetchAll(PDO::FETCH_ASSOC);
+            $informations = json_encode($informations);
+            return $informations;
+        }else{
+            error_log("Die Funktion sqlPreparedStatement() konnte nicht ausgeführt werden!");
+            return null;
+        }
+    }
+
+    public function sqlPreparedStatementWithAjaxName($sqlStatement, $bindParams, $ajaxname){
+
+        $pdo = $this->pdo;
+
+        $statement = $pdo->prepare($sqlStatement);
+
+        foreach ($bindParams as $key => &$value){
+            $statement->bindParam(":$key", $value);
+        }
+
+        if($statement->execute()){
+
+            $informations = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+            $output[$ajaxname] = $informations;
+
+            $output = json_encode($output);
+
+            return $output;
+        }else{
+            return null;
+        }
+    }
+}
